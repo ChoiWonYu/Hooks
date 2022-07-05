9f08b7f33c9d33a445a97226d2ae00aaf1590754
ChoiWonYu <93238308+ChoiWonYu@users.noreply.github.com>
2022-07-05 20:24:27 +0900

    Add files via upload

---
+++ useBeforeLeave.js
@@ -0,0 +1,10 @@
+export const useBeforeLeave =(onBefore)=>{
+    const handle=(e)=>{
+      const {clientY}=e;
+      if(clientY<=0)onBefore();
+    }
+    useEffect(()=>{
+        document.addEventListener('mouseleave',handle);
+        return()=>document.removeEventListener('mouseleave',handle);
+    },[]);
+  }
